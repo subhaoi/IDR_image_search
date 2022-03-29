ec67e54ff5aa672a9c5863eb17f6fe045e364b94
subhash <subhaoi@gmail.com>
2022-03-29 08:57:45 +0530

    adding copying file

---
+++ copying_files.sh
@@ -0,0 +1,20 @@
+ssh idron8577@49.12.7.186 -p5522
+
+username : idron8577
+password : mhBWhhGlkiaS
+
+sshpass -p 'mhBWhhGlkiaS' scp -r -P 5522 idron8577@49.12.7.186:~/public_html/wp-content/uploads/2019/ .
+
+
+mv 12/* ../all/.
+mv 11/* ../all/.
+mv 10/* ../all/.
+mv 09/* ../all/.
+mv 08/* ../all/.
+mv 07/* ../all/.
+mv 06/* ../all/.
+mv 05/* ../all/.
+mv 04/* ../all/.
+mv 03/* ../all/.
+mv 02/* ../all/.
+mv 01/* ../all/.
